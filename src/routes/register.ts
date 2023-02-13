f5ef3d5eb62ea700075b7b88e7fe4e9f0fbb5e6d
Ludvig-Lundberg <ickeskola@gmail.com>
2023-02-13 12:01:52 +0100

    lagt till validering på register EMAIL

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,14 +1,21 @@
-/**
- * Router Template
- */
 import express from 'express'
 import { body } from 'express-validator'
 import { register } from '../controllers/register_controller'
+import prisma from '../prisma'
 const router = express.Router()
 
-// lägg till photo
+// lägg till användare
 router.post('/', [
-    
+    body("email").isEmail().bail().custom(async (email:string) => {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email,
+            }
+        })
+        if (user) {
+            return Promise.reject("Email already in use")
+        }
+    })
 ], register)
 
 

dab3b3daa89d3b0fe40ebcad441bba2ac15fbcc5
Ludvig-Lundberg <ickeskola@gmail.com>
2023-02-22 10:21:37 +0100

    lagt uppdaterad SQL-dump i roten av repo:t och tagit bort migrationsmappen för problem med cyclic

--- bbprwmpukf3zu7k4tdiz.sql
+++ bbprwmpukf3zu7k4tdiz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: bbprwmpukf3zu7k4tdiz-mysql.services.clever-cloud.com:3306
--- Generation Time: Feb 20, 2023 at 12:22 PM
+-- Generation Time: Feb 22, 2023 at 09:14 AM
 -- Server version: 8.0.22-13
 -- PHP Version: 7.2.34
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `album` (
   `id` int UNSIGNED NOT NULL,
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` int UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -39,7 +39,12 @@ CREATE TABLE `album` (
 --
 
 INSERT INTO `album` (`id`, `title`, `user_id`) VALUES
-(1, 'Mitt #1 Album', 1);
+(1, 'Mitt #1 Album', 1),
+(2, 'Mitt #2 Album', 1),
+(3, '2 ALBUM BLUM Album', 2),
+(4, '4th Album 3rd ID', 3),
+(5, 'Mitt #2 Album', 3),
+(6, 'Mitt #3 Album', 3);
 
 -- --------------------------------------------------------
 
@@ -49,9 +54,9 @@ INSERT INTO `album` (`id`, `title`, `user_id`) VALUES
 
 CREATE TABLE `photo` (
   `id` int UNSIGNED NOT NULL,
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `comment` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `url` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `comment` text COLLATE utf8mb4_unicode_ci,
   `user_id` int UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -60,11 +65,14 @@ CREATE TABLE `photo` (
 --
 
 INSERT INTO `photo` (`id`, `title`, `url`, `comment`, `user_id`) VALUES
-(1, 'Saddadad Photo', 'https://images.undwadadawsplash.com/photo-1454486837617-ce8e1ba5ebfe', 'Sdwadwada', 1),
-(2, 'Happy Photo', 'https://images.undwadadawsplash.com/photo-1454486837617-ce8e1ba5ebfe', 'HAPPY AGAIN!!!!! :))))', 1),
-(3, 'wdat ad wadcadcasf wdadadawcda221122', 'https://images.undwadadawsplash.com/photo-1454486837617-ce8e1ba5ebfe', 'ah yes more comments', 1),
-(4, 'WEEEEEEEEEEEEEEEEEEE', 'https://images.undwadadawsplash.com/photo-1454486837617-ce8e1ba5ebfe', 'waewadwawdawents', 2),
-(5, 'WEEEEEEEEEEEEEEEEEEE', 'https://images.undwadadawsplash.com/photo-1454486837617-ce8e1ba5ebfe', 'waewadwawdawents', 1);
+(1, 'WEEEEEEEEEEEEEEEEEEE', 'https://images.undwadadawsplash.com/photo-1454486837617-ce8e1ba5ebfe', 'waewadwawdawents', 1),
+(2, 'wdat ad wadcadcasf wdadadawcda221122', 'https://images.undwadadawsplash.com/photo-1454486837617-ce8e1ba5ebfe', 'waewadwawdawents', 1),
+(3, 'WEEEEEEEEEEEEEEEEEEE', 'https://images.undwadadawsplash.com/photo-1454486837617-ce8e1ba5ebfe', 'waewadwawdawents', 1),
+(4, 'WEEEEEEEEEEEEEEEEEEE', 'https://images.undwadadawsplash.com/photo-1454486837617-ce8e1ba5ebfe', 'waewadwawdawents', 1),
+(5, '1213', 'hekpa.co', 'gawd', 3),
+(6, 'WEEEEEEEEEEEEEEEEEEE', 'https://images.undwadadawsplash.com/photo-1454486837617-ce8e1ba5ebfe', 'waewadwawdawents', 3),
+(7, 'WEEEEEEEEEEEEEEEEEEE', 'https://images.undwadadawsplash.com/photo-1454486837617-ce8e1ba5ebfe', 'waewadwawdawents', 3),
+(8, 'AWDWADAW', 'h22magesund.wadadawspfe', 'awdwa', 3);
 
 -- --------------------------------------------------------
 
@@ -74,10 +82,10 @@ INSERT INTO `photo` (`id`, `title`, `url`, `comment`, `user_id`) VALUES
 
 CREATE TABLE `user` (
   `id` int UNSIGNED NOT NULL,
-  `email` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `first_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `last_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -85,43 +93,35 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `password`, `first_name`, `last_name`) VALUES
-(1, 'ickeskola@gmail.com', '$2b$10$nV0.uqnVkm9Zg/zS.7AYZO7OaVoXHGYiIOIeZV9SmjRU7lLUqfOjG', 'Ludvig', 'Lundberg'),
-(2, 'ickeskola2@gmail.com', '$2b$10$9eZtE6PF8gokPgMg41QJXuyMn3vZsZRH9Sew4wy3JF6dpb2Nu6qK2', 'Ludvig', 'Lundberg');
+(1, 'ickeskola@gmail.com', '$2b$10$G3alfOv/TnPajIHZQcvEIurxqGHUpzrNu8012.vg4mFIMw/Y1bk2m', 'Ludvig', 'Lundberg'),
+(2, 'ickeskola2@gmail.com', '$2b$10$EzKWq8ALl3BGWfChetVMdeQmlDU2Z.FTn5oHJKkV3E3TIcvtDEdyO', 'Ludvig', 'Lundberg'),
+(3, 'ickeskola3@gmail.com', '$2b$10$wNB3YeH6RMUHcBf2g2n1SuLGu91mpSWcvlelsREfVj0q4Ty0zEYyK', 'Ludvig', 'Lundberg'),
+(4, 'l@gmail.com', '$2b$10$wiJ00kKWpzKPAPq4jfmX7.Txuj/Cm6XGdnOfNHW8TH4EiNuajvfPK', 'Lugg', 'Ligg'),
+(5, 'example@email.com', '$2b$10$oj1rN0jJka9.EnT7bauSFefQkhFixZkU/NONKanSfREhziMx80uSC', 'first name', 'last name');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `_albumtophoto`
+-- Table structure for table `_albumTophoto`
 --
 
-CREATE TABLE `_albumtophoto` (
+CREATE TABLE `_albumTophoto` (
   `A` int UNSIGNED NOT NULL,
   `B` int UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `_prisma_migrations`
+-- Dumping data for table `_albumTophoto`
 --
 
-CREATE TABLE `_prisma_migrations` (
-  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `checksum` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `finished_at` datetime(3) DEFAULT NULL,
-  `migration_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `logs` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-  `rolled_back_at` datetime(3) DEFAULT NULL,
-  `started_at` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-  `applied_steps_count` int UNSIGNED NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `_prisma_migrations`
---
-
-INSERT INTO `_prisma_migrations` (`id`, `checksum`, `finished_at`, `migration_name`, `logs`, `rolled_back_at`, `applied_steps_count`) VALUES
-('d3e04620-de1b-4616-9e36-13513224a755', '5f84e6fe2c87cb443852240ec0e53be2f19c670c5df7fed9303ea113ab503240', '2023-02-20 11:45:17.031', '20230217122555_first_migrate', NULL, NULL, 1);
+INSERT INTO `_albumTophoto` (`A`, `B`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(4, 5),
+(4, 6),
+(4, 8);
 
 --
 -- Indexes for dumped tables
@@ -149,17 +149,11 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `user_email_key` (`email`);
 
 --
--- Indexes for table `_albumtophoto`
+-- Indexes for table `_albumTophoto`
 --
-ALTER TABLE `_albumtophoto`
+ALTER TABLE `_albumTophoto`
   ADD UNIQUE KEY `_albumTophoto_AB_unique` (`A`,`B`),
   ADD KEY `_albumTophoto_B_index` (`B`);
-
---
--- Indexes for table `_prisma_migrations`
---
-ALTER TABLE `_prisma_migrations`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -169,19 +163,19 @@ ALTER TABLE `_prisma_migrations`
 -- AUTO_INCREMENT for table `album`
 --
 ALTER TABLE `album`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `photo`
 --
 ALTER TABLE `photo`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -191,18 +185,18 @@ ALTER TABLE `user`
 -- Constraints for table `album`
 --
 ALTER TABLE `album`
-  ADD CONSTRAINT `album_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `album_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 --
 -- Constraints for table `photo`
 --
 ALTER TABLE `photo`
-  ADD CONSTRAINT `photo_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `photo_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 --
--- Constraints for table `_albumtophoto`
+-- Constraints for table `_albumTophoto`
 --
-ALTER TABLE `_albumtophoto`
+ALTER TABLE `_albumTophoto`
   ADD CONSTRAINT `_albumTophoto_A_fkey` FOREIGN KEY (`A`) REFERENCES `album` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `_albumTophoto_B_fkey` FOREIGN KEY (`B`) REFERENCES `photo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
